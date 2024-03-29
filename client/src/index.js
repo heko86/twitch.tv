1eeb8e085b6c59bc55027b99ba4bf72f277150b3
heko86 <ryou86pg@gmail.com>
2024-03-30 00:17:27 +0900

    ページ遷移の実装

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App";
+import { DashboardPage } from "./DashboardPage";
+import { AuthPage } from "./AuthPage";
+
 import "./index.css";
-import App from "./App";
+
+const router = createBrowserRouter([
+  { path: "auth", element: <AuthPage /> },
+  {
+    path: "/",
+    element: <DashboardPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>
 );
