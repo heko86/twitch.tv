d185893a180a79d9a6b3844dbabfae303e866a58
heko86 <ryou86pg@gmail.com>
2024-04-02 17:33:33 +0900

    ユーザー情報取得APIの実装

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./src/routes/authRoutes.js";
+import channelsRoutes from "./src/routes/channelsRoutes.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/channels", channelsRoutes);
 
 const server = http.createServer(app);
 
---
+++ server/src/routes/channelsRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import Joi from "joi";
+import ExpressValidation from "express-joi-validation";
+import { getChannelDetails } from "../controllers/controllers.js";
+
+const router = express.Router();
+
+const channelDetailsSchema = Joi.object({
+  channelId: Joi.string().required(),
+});
+
+const validator = ExpressValidation.createValidator({});
+
+router.get(
+  "/:channelId",
+  validator.params(channelDetailsSchema),
+  getChannelDetails
+);
+export default router;
