3696787a8ed74cb9b602d8940f630233c45b1f90
heko86 <ryou86pg@gmail.com>
2024-04-09 15:03:45 +0900

    react-flv-playerの導入

--- client/src/DashboardPage/Content/ChannelView/ChannelView.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelView.js
@@ -4,6 +4,19 @@ import { ChannelDescription } from "./ChannelDescription";
 import { useChannelDetails } from "../../../shared/hooks";
 import { useParams } from "react-router-dom";
 import { LoadingSpinner } from "../../../shared/component";
+import { ReactFlvPlayer } from "react-flv-player";
+
+export const Stream = () => {
+  return (
+    <div className="channel-video-container">
+      <ReactFlvPlayer
+        width="100%"
+        height="100%"
+        url="http://localhost:8000/live/10a97658-f438-495e-b7a2-1d495c683f33.flv"
+      />
+    </div>
+  );
+};
 
 export const ChannelView = ({ getChannels }) => {
   const { isFetching, channelDetails, getChannelDetails } = useChannelDetails();
@@ -20,9 +33,10 @@ export const ChannelView = ({ getChannels }) => {
   return (
     <div className="channel-container">
       <div className="channel-video-description-section">
-        <div className="channel-offline-placeholder">
+        {/* <div className="channel-offline-placeholder">
           <span>Channel is Offline</span>
-        </div>
+        </div> */}
+        <Stream />
         <ChannelDescription
           channelId={channelDetails.id}
           username={channelDetails.username}
